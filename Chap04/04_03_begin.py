3c01bcf022f4d16228f7ea82d31f06be1489311b
smejia <smejia@qualtrics.com>
2024-09-22 22:32:23 -0600

    Commit1

---
+++ Chap04/04_03_begin.py
@@ -0,0 +1,8 @@
+plant = "Oak"
+
+if plant == "Cacti":
+    print(plant, "don't need a lot of water")
+else:
+    print(plant, "love water")
+
+print("Thanks!")
---
+++ Chap04/Excercise_Guessing_Game.py
@@ -0,0 +1,9 @@
+Food = input("What's your favorite food?")
+
+food = "Lasagna"
+if food == Food:
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That's not it!")
+
+print ("Thanks for playing")
